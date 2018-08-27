c8f961714742e743998e061e38d6ce44dbbd2a72
Sheth <Charmik.Sheth@vantiv.com>
2018-08-27 10:40:13 -0400

    Added unit test for MCCRetrievalRequest

---
+++ PayFacMpSDK/PayFacMpSDKTest/Unit/TestAgreementRetrievalRequest.cs
@@ -0,0 +1,48 @@
+﻿using Moq;
+using NUnit.Framework;
+using PayFacMpSDK;
+
+namespace PayFacMpSDKTest.Unit
+{
+    [TestFixture]
+    public class TestAgreementRetrievalRequest
+    {
+        private AgreementRetrievalRequest request;
+        private legalEntityAgreementRetrievalResponse response;
+
+        [SetUp]
+        public void setUp()
+        {
+            request = new AgreementRetrievalRequest();
+        }
+
+        [Test]
+        public void TestGetLegalEntityAgreementRequest()
+        {
+            var expectedResposne = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n" +
+                                   "<legalEntityAgreementRetrievalResponse xmlns=\"http://payfac.vantivcnp.com/api/merchant/onboard\">\n" +
+                                   "    <legalEntityId>2010001</legalEntityId>\n" +
+                                   "    <transactionId>3187832775</transactionId>\n" +
+                                   "    <agreements>\n" +
+                                   "        <legalEntityAgreement>\n" +
+                                   "            <legalEntityAgreementType>MERCHANT_AGREEMENT</legalEntityAgreementType>\n" +
+                                   "            <agreementVersion>agreementVersion1</agreementVersion>\n" +
+                                   "            <userFullName>userFullName1</userFullName>\n" +
+                                   "            <userSystemName>userSystemName1</userSystemName>\n" +
+                                   "            <userIPAddress>196.198.100.100</userIPAddress>\n" +
+                                   "            <manuallyEntered>false</manuallyEntered>\n" +
+                                   "            <acceptanceDateTime>2017-06-11T13:00:00-05:00</acceptanceDateTime>\n" +
+                                   "        </legalEntityAgreement>\n" +
+                                   "    </agreements>\n" +
+                                   "</legalEntityAgreementRetrievalResponse>";
+            var mock = new Mock<Communication>();
+            mock.Setup(Communication => Communication.Get("/legalentity/2010001/agreement")).Returns(expectedResposne);
+
+            Communication communicationMock = mock.Object;
+            request.Communication = communicationMock;
+
+            response = request.GetLegalEntityAgreementRequest("2010001");
+            
+        }
+    }
+}
---
+++ PayFacMpSDK/PayFacMpSDKTest/Unit/TestMCCRetrievalRequest.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using Moq;
+using PayFacMpSDK;
+using PayFacMpSDK.Properties;
+
+namespace PayFacMpSDKTest.Unit
+{
+    [TestFixture]
+    public class TestMCCRetrievalRequest
+    {
+        private MCCRetrievalRequest request;
+        private approvedMccResponse response;
+
+        [SetUp]
+        public void setUp()
+        {
+            request = new MCCRetrievalRequest();
+        }
+
+        [Test]
+        public void TestGetMCCRequest()
+        {
+            string expectedResposne = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>"+
+                "<approvedMccResponse xmlns=\"http://payfac.vantivcnp.com/api/merchant/onboard\">"+
+                    "<transactionId>0190924847</transactionId>"+
+                    "<approvedMccs>"+
+                    "<approvedMcc>5967</approvedMcc>"+
+                    "<approvedMcc>5970</approvedMcc>"+
+                    "</approvedMccs>"+
+                "</approvedMccResponse>";
+            var mock = new Mock<Communication>();
+            mock.Setup(Communication => Communication.Get("/mcc")).Returns(expectedResposne);
+
+            Communication communicationMock = mock.Object;
+            request.Communication = communicationMock;
+            response = request.GetMCCRequest();
+            Assert.AreEqual("5967", response.approvedMccs[0]);
+            Assert.AreEqual("5970", response.approvedMccs[1]);
+            Assert.AreEqual(190924847, response.transactionId);
+        }
+
+        [Test]
+        public void TestGettersAndSetters()
+        {
+            // Configuration
+            var config = new Configuration();
+            request.Configuration = config;
+            Assert.AreSame(config, request.Configuration);
+            
+            // Communication
+            var comm = new Communication();
+            request.Communication = comm;
+            Assert.AreSame(comm, request.Communication);
+        }
+    }
+}
